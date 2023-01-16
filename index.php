bc23d91f8bfe3ba3dd613e61d3fdcd94f9d650c9
issam <issammez44@gmail.com>
2023-01-16 15:41:09 +0100

    add the home page

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./src/assets/css/main.css">
-    <title>Planet DEV</title>
-</head>
-<body>
-    <h1>hello world!</h1>
-</body>
-</html>
+<?php include('./src/includes/header.php') ?>
+
+<div class="hero min-h-screen" style="background-image: url('https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=847&q=80');">
+    <div class="hero-overlay bg-opacity-60"></div>
+    <div class="hero-content text-center text-neutral-content">
+        <div class="max-w-md">
+            <h1 class="mb-5 text-5xl font-bold">Planet DEV</h1>
+            <p class="mb-5">Stay ahead of the curve by reading the latest tech news on our website, where we bring you the most cutting-edge information on the industry.</p>
+            <button class="btn btn-primary" onClick="window.location.href='./src/views/login.php'">Get Started</button>
+        </div>
+    </div>
+</div>
+
+<?php include('./src/includes/footer.php') ?>
---
+++ src/includes/footer.php
@@ -0,0 +1,4 @@
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="../assets/js/main.js"></script>
+</body>
+</html>
